e905e569077502527fed082323e4f1eb624d5bfa
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-07-15 15:43:54 +0200

    define total_unused storage

--- check_diskspace_total.sh
+++ check_diskspace_total.sh
@@ -1,7 +1,7 @@
 #Disk space monitoring script: Check disk usage and send alerts if it exceeds a certain threshold.
 
-total_used_storage=$(df -h --total | grep "total" | awk '{print $5}')
-echo $total_used_storage
+total_used_storage=$(df -h --total | grep "total" | awk '{print $5}'| sed 's/%//')
+echo "The percentage of total used storage : $total_used_storage%"
 
-total_unused_storage=$(df -h --total | grep "total" | awk '{print $5}')
-echo $total_unused_storage
+total_unused_storage=$((100-$total_used_storage))
+echo "The percentage of total unused storage : $total_unused_storage%"
