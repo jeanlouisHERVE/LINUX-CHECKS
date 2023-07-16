90449d799bccf31c93079ce5c8e119a9da167c3a
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-07-16 11:11:28 +0200

    add variables to check_memory

--- check_memory.sh
+++ check_memory.sh
@@ -1,1 +1,6 @@
 #Memory usage script: Check the amount of free memory available and alert if it falls below a specified limit.
+
+total_memory_space=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}'| sed 's/kB//')
+echo "$total_memory_space"
+free_memory_space=$(cat /proc/meminfo | grep MemFree | awk '{print $2}'| sed 's/kB//')
+echo "$free_memory_space"
