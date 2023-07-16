de5f78462f8a6dd89b7d750d6aa36d3cbb95575e
Jean-Louis HERVE <jeanlouis.herve@hotmail.fr>
2023-07-16 18:47:28 +0200

    add note to cpu script

--- check_cpu.sh
+++ check_cpu.sh
@@ -1,1 +1,28 @@
 #CPU load monitoring script: Monitor CPU usage and notify if it reaches a critical level.
+
+# CPU Usage: Monitor the overall CPU usage to understand its workload. You can use tools like top, htop, or glances 
+# to view the CPU usage in real-time. These tools provide a breakdown of CPU usage by individual processes and threads, 
+# helping you identify resource-intensive applications.
+
+# CPU Frequency and Scaling: Check the CPU frequency to determine if the processor is running at its maximum capability
+#  or if it's being scaled down to conserve power. The cpufreq-info or lscpu commands can provide information on CPU 
+#  frequency scaling.
+
+# CPU Temperature: Monitor the CPU temperature to ensure it's operating within safe limits. Excessive heat can affect
+#  performance and lead to thermal throttling. Use tools like sensors or lm-sensors to retrieve CPU temperature readings.
+
+# CPU Model and Features: Identify the specific model and features of your CPU. The lscpu command provides detailed 
+# information about the CPU architecture, vendor, cores, cache sizes, and more.
+
+# CPU Load Average: Examine the system's load average, which represents the average number of processes waiting to 
+# be executed over different time intervals. You can view the load average using the uptime command or by reading the /proc/loadavg file.
+
+# Interrupts and Context Switches: Monitor the number of interrupts and context switches occurring on the CPU. Tools
+#  like vmstat or sar can display these statistics, providing insights into system performance and potential bottlenecks.
+
+# CPU Performance Counters: Utilize performance monitoring tools like perf to access CPU performance counters. These
+#  counters provide low-level details on CPU events, such as cache misses, branch predictions, and instructions retired,
+#  helping you analyze and optimize performance-critical code.
+
+# CPU Affinity: Explore CPU affinity settings to control which CPUs are utilized by specific processes or threads.
+# The taskset command allows you to assign or query CPU affinity for a given process.
