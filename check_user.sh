a062e11d247f3c7bd43f78f6afbcff31efca0940
jeanlouisHERVE <jeanlouis.herve@hotmail.fr>
2023-07-19 18:05:54 +0200

    improve check_users script

--- check_user.sh
+++ check_user.sh
@@ -1,11 +1,18 @@
 #User account monitoring script: Monitor user account activity and report any unauthorized access attempts.
 
 #sudo apt install acct
+##commands
+#ac : find number of hours 
 
 #variable 
 main_user="jean-louis"
-find_users=""
+find_users=$(ac -p | grep -v -e $main_user -e "total")
+number_extra_users=0
 
 ###check if another user was connected 
-find_users=$(ac -p | grep -v -e $main_user -e "total") 
-echo "$find_users"
+number_extra_users=$($find_users | wc -l ) 
+
+if [ $number_extra_users -ne 0] then; 
+    exit 1 
+else
+    exit 0
