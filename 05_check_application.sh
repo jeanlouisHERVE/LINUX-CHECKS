19ebf5e151960629456edf45ea18dbb9dba76567
HERVE <jean-louis.herve.external@worldline.com>
2023-07-30 07:51:46 +0200

    correct elif statement check status to check application

--- 05_check_application.sh
+++ 05_check_application.sh
@@ -6,10 +6,10 @@ status_output=$(sudo systemctl status $app_name)
 if echo "$status_output" | grep -q "Active: active (running)"; then
     echo "[INFO]  >> application: $app_name << is active and running"
     exit 0
-elif echo "$status_output" | grep -q "Active: failed (Result: start-limit-hit)";
+elif echo "$status_output" | grep -q "Active: failed (Result: start-limit-hit)"; then
     echo "[DANGER]  >> application: $app_name << failed to start"
     exit 1
-elif echo "$status_output" | grep -q "Active: inactive (dead)";
+else echo "$status_output" | grep -q "Active: inactive (dead)";
     echo "[DANGER]  >> application: $app_name << is inactive"
     exit 1
 fi
