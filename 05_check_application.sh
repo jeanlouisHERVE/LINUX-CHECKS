466b7423e6c63dba88cde7f0ce79581d5056feee
HERVE <jean-louis.herve.external@worldline.com>
2023-07-30 07:50:11 +0200

    complete check status to check application

--- 05_check_application.sh
+++ 05_check_application.sh
@@ -4,9 +4,12 @@ app_name="application"
 status_output=$(sudo systemctl status $app_name)
 
 if echo "$status_output" | grep -q "Active: active (running)"; then
-    echo "[INFO]  >> application: $app_name << is running"
+    echo "[INFO]  >> application: $app_name << is active and running"
     exit 0
-else
-    echo "[DANGER]  >> application: $app_name << is stopped"
+elif echo "$status_output" | grep -q "Active: failed (Result: start-limit-hit)";
+    echo "[DANGER]  >> application: $app_name << failed to start"
+    exit 1
+elif echo "$status_output" | grep -q "Active: inactive (dead)";
+    echo "[DANGER]  >> application: $app_name << is inactive"
     exit 1
 fi
